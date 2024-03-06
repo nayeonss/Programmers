ef87e4a3331ce1d0abcff9f0d3541260de873fc9
nayeonss <118796565+nayeonss@users.noreply.github.com>
2024-03-07 02:59:25 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,8 +1,5 @@
 def solution(a, b):
     a = str(a)
     b = str(b)
-    if int(a + b) >= int(b + a):
-        answer = int(a + b)
-    else:
-        answer = int(b + a)
+    answer = int(max(a+b, b+a))
     return answer
